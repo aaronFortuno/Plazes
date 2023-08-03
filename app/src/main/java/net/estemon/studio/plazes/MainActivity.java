302d8c0aabf3d34eaec2e6e8b733806e4202135d
aaronFortuno <aaron.fortuno@protonmail.com>
2023-08-03 17:47:05 +0200

    - initial layout - ScrollView added

--- app/src/main/java/net/estemon/studio/plazes/MainActivity.java
+++ app/src/main/java/net/estemon/studio/plazes/MainActivity.java
@@ -1,14 +1,41 @@
 package net.estemon.studio.plazes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import net.estemon.studio.plazes.databinding.ActivityScrollingBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityScrollingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
