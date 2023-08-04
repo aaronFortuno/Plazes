4c491976226369dea6d43c24b4b38488709d9c85
aaronFortuno <aaron.fortuno@protonmail.com>
2023-08-04 07:52:31 +0200

    - AboutActivity and activity_about.xml - Drawable assets

--- app/src/main/java/net/estemon/studio/plazes/MainActivity.java
+++ app/src/main/java/net/estemon/studio/plazes/MainActivity.java
@@ -1,6 +1,10 @@
 package net.estemon.studio.plazes;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AboutActivity aboutActivity = new AboutActivity();
+        Button about = findViewById(R.id.btn_about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAppActivity(aboutActivity);
+            }
+        });
+    }
+
+    public void startAppActivity(Activity activity) {
+        Intent intent = new Intent(this, activity.getClass());
+        startActivity(intent);
     }
 }
